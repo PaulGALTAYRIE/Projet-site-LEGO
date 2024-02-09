1435d0c4b5abf4d0b5bf6e7513f4d6c9acb01118
PaulGALTAYRIE <paul.galtayrie@etu.estia.fr>
2024-02-09 18:09:44 +0100

    Nouvelle DataBase
    Il y a deux utilisateurs :
    User: Dieu/ mdp: 1234/ statut: 1
    User: Jesus/ mdp: 0000/ statut: 0

--- DB/lego_web_site.sql
+++ DB/lego_web_site.sql
@@ -2,9 +2,9 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3307
--- Généré le : mer. 31 jan. 2024 à 09:16
--- Version du serveur : 10.10.2-MariaDB
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 09 fév. 2024 à 17:06
+-- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -29,14 +29,21 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `commande`;
 CREATE TABLE IF NOT EXISTS `commande` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_livreur` int(11) NOT NULL,
-  `id_utilisateur` int(11) NOT NULL,
-  `statut` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_livreur` int NOT NULL,
+  `id_utilisateur` int NOT NULL,
+  `statut` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_livreur` (`id_livreur`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `id_livreur`, `id_utilisateur`, `statut`) VALUES
+(1, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -46,13 +53,20 @@ CREATE TABLE IF NOT EXISTS `commande` (
 
 DROP TABLE IF EXISTS `livreur`;
 CREATE TABLE IF NOT EXISTS `livreur` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nom` text NOT NULL,
   `region_livraison` text NOT NULL,
   `temps_livraison` text NOT NULL,
-  `prix` int(11) NOT NULL,
+  `prix` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `livreur`
+--
+
+INSERT INTO `livreur` (`id`, `nom`, `region_livraison`, `temps_livraison`, `prix`) VALUES
+(1, 'amazon', 'monde', '2 jours', 5);
 
 -- --------------------------------------------------------
 
@@ -62,13 +76,20 @@ CREATE TABLE IF NOT EXISTS `livreur` (
 
 DROP TABLE IF EXISTS `ordre`;
 CREATE TABLE IF NOT EXISTS `ordre` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_commande` int(11) NOT NULL,
-  `id_piece` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_commande` int NOT NULL,
+  `id_piece` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_commande` (`id_commande`),
   KEY `id_piece` (`id_piece`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `ordre`
+--
+
+INSERT INTO `ordre` (`id`, `id_commande`, `id_piece`) VALUES
+(1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -78,14 +99,21 @@ CREATE TABLE IF NOT EXISTS `ordre` (
 
 DROP TABLE IF EXISTS `piece`;
 CREATE TABLE IF NOT EXISTS `piece` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nom` text NOT NULL,
-  `quantite` int(11) NOT NULL,
+  `quantite` int NOT NULL,
   `format` text NOT NULL,
   `couleur` text NOT NULL,
-  `prix` int(11) NOT NULL,
+  `prix` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `piece`
+--
+
+INSERT INTO `piece` (`id`, `nom`, `quantite`, `format`, `couleur`, `prix`) VALUES
+(1, 'Brick 2x3 red', 1, '2x3', 'red', 2);
 
 -- --------------------------------------------------------
 
@@ -95,19 +123,26 @@ CREATE TABLE IF NOT EXISTS `piece` (
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_ordre` int(11) NOT NULL,
-  `email` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
   `mdp` text NOT NULL,
-  `number` int(11) NOT NULL,
+  `number` int NOT NULL,
   `adresse` text NOT NULL,
   `specification` text NOT NULL,
-  `code_postale` int(11) NOT NULL,
+  `code_postale` int NOT NULL,
   `country` text NOT NULL,
-  `statut` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_ordre` (`id_ordre`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `statut` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `name`, `email`, `mdp`, `number`, `adresse`, `specification`, `code_postale`, `country`, `statut`) VALUES
+(1, 'Dieu', 'Dieu@Paradis.world', '1234', 422521010, '13 rue de judas', 'Déposer sur le paillasson à l\'entrée', 77777, 'Paradis', 1),
+(2, 'Jesus', 'Jesus@Paradis.world', '0000', 185099550, '13 Jardin d’Éden ', 'Ne mangez pas les pommes !', 77777, 'Terre', 0);
 
 --
 -- Contraintes pour les tables déchargées
@@ -126,12 +161,6 @@ ALTER TABLE `commande`
 ALTER TABLE `ordre`
   ADD CONSTRAINT `ordre_ibfk_1` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id`),
   ADD CONSTRAINT `ordre_ibfk_2` FOREIGN KEY (`id_piece`) REFERENCES `piece` (`id`);
-
---
--- Contraintes pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`id_ordre`) REFERENCES `ordre` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
